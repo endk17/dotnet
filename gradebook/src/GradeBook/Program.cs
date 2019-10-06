1c813322754fb718a3ee184de57d3f11d5a89c5e
Enda Kelly <ekel@Endas-MacBook-Pro.local>
2019-10-06 19:40:20 +0100

    EK c# calc

---
+++ gradebook/src/GradeBook/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book("TEST Grades");
+            book.AddGrade(35.1);
+            book.AddGrade(95.9);
+            book.AddGrade(77.5);
+            book.showstatistics();
+        }
+    }
+}
